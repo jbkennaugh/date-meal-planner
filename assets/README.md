819982171ee22da073b4f48125743ddb989bda21
joseph akah <josephakah@macbook-pro-2.lan>
2023-02-11 18:07:25 +0000

    movies html and JS and updated css with readme update for movies html with additional updates

---
+++ assets/README.md
@@ -0,0 +1,56 @@
+
+# Movie App-rapid Api
+In this app user can search and  sort out a movie woth names and ratings aslo.It is created with rapid api.
+
+## Getting Started
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+- GitHub Repository
+- Deployed GitHub IO
+
+
+
+## Installing
+
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository.
+
+
+- An interactive, timed JavaScript quiz that stores a high score leaderboard in local storage.
+Developed in accordance with the following acceptance criteria: 
+
+
+
+
+
+## Built with
+* HTML
+* CSS
+* JavaScript
+* BootStrap
+* jQuery
+* moments.js
+* FontAwesome
+* Google Fonts
+
+## Authors
+
+- ##  Helpfull Links
+
+ - [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+ - [Version Control](https://en.wikipedia.org/wiki/Version_control)
+ - [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+ - [Dev Docs](https://devdocs.io/)
+- [Bootstrap Documentation](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
+
+- [Responsive Web Design - How to Create Media Queries](https://www.youtube.com/watch?v=5xzaGSYd7jM)
+
+- [Media Queries 101 by CSS Tricks](https://css-tricks.com/css-media-queries/)
+
+- [Media Query Documentation](https://www.w3schools.com/css/css_rwd_mediaqueries.asp)
+
+
+
+## Contact 
+
+If you have any feedback, please reach out to us at facebook@mail.com
+
--- assets/js/movies.js
+++ assets/js/movies.js
@@ -5,18 +5,111 @@ const options = {
 		'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
 	}
 };
-var content=document.getElementById("movies");
+var content = document.getElementById("movies");
 
+const input = document.getElementById("input");
 
-fetch('https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest&page=1', options)
-	.then(response => response.json())
-	.then(response => {
-        const list=response.results;
+function Seach(e) {
+	console.log(input.value);
+	content.innerHTML = ''
+	fetch(`https://ott-details.p.rapidapi.com/search?title=${input.value}&page=1`, options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response.results);
+			const list = response.results;
 
-        list.map((item)=>{
-            console.log(item);
-            content.innerHTML+=`
+			list.map((item) => {
+				console.log(item);
+
+				content.innerHTML += `
             <img src="${item.imageurl}" alt="">`;
-        })
-    })
-	.catch(err => console.error(err));    
+			})
+		})
+		.catch(err => console.error(err));
+
+}
+function genreItem(params) {
+	params.preventDefault();
+	console.log('hi');
+
+	
+}
+
+function getGenre(i) {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'c600bf7772msh077aac853ada820p13f288jsn60ccf5a97a3b',
+			'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+		}
+	};
+
+	fetch('https://ott-details.p.rapidapi.com/getParams?param=genre', options)
+		.then(response => response.json())
+		.then(response => {
+			const genreOne= response.slice(0,10);
+			const genretwo= response.slice(11,20);
+			const genreThree= response.slice(20,26);
+			genreOne.map((i) => {
+				console.log(i);
+				const genreOne = document.getElementById("genreOne");
+				genreOne.innerHTML += `
+				<a href="" onclick='genreItem()'>${i}</a> <br>
+				`;
+
+			})
+			genretwo.map((i) => {
+				console.log(i);
+				const genreTwo = document.getElementById("genreTwo");
+				genreTwo.innerHTML += `
+				<a href="">${i}</a> <br>
+				`;
+
+			})
+			genreThree.map((i) => {
+				console.log(i);
+				const genreThree = document.getElementById("genreThree");
+				genreThree.innerHTML += `
+				<a href="">${i}</a> <br>
+				`;
+
+			})
+			
+			console.log(object);
+		})
+		.catch(err => console.error(err));
+
+}
+function searchgenre(params) {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'c600bf7772msh077aac853ada820p13f288jsn60ccf5a97a3b',
+			'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+		}
+	};
+	
+	fetch(`https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=${'action'}&language=english&type=movie&sort=latest&page=1`, options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response.results);
+			const list = response.results;
+
+			list.map((item) => {
+				console.log(item);
+
+				content.innerHTML += `
+            <img src="${item.imageurl}" alt="">`;
+			})
+
+		})
+		.catch(err => console.error(err));
+	
+}
+
+
+
+
+
+
+
